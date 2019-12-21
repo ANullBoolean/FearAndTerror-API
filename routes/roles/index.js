2cffe98e68fd1d42c53ddd57a2bb70a585a10bfe
Bryan Gillespie <bryan.gillespie@accpl.co>
2019-12-20 21:08:51 -0500

    guilds for FaT only

--- routes/roles/index.js
+++ routes/roles/index.js
@@ -3,6 +3,9 @@ const { Roles } = require('../../models/Roles');
 const getRoles = (req, res) => {
   Roles.findAll({
     ...req.pagination,
+    where: {
+      guild: "398543362476605441",
+    }
   }).then(result => {
     res.status(200).send(result);
   });
