9be8ab3d989bf22b539e3cc945fe205da73f9ccf
Bryan Gillespie <bryan.gillespie@accpl.co>
2020-01-30 15:51:04 -0500

    Division voice users by hour

--- routes/voice/index.js
+++ routes/voice/index.js
@@ -43,12 +43,6 @@ const getChannelVoiceActivity = (req, res) => {
   const channelId = req.params.channelId;
 
   VoiceActivity.findAll({
-    attributes: [
-      [ VoiceActivity.sequelize.literal(`DATE("jointime")`), 'date' ],
-      [ VoiceActivity.sequelize.literal(`SUM("time")`), 'time' ],
-      [ VoiceActivity.sequelize.literal('COUNT(DISTINCT("userId"))'), 'users' ],
-    ],
-    group: ['date'],
     where: {
       createdAt: {
         [Op.gte]: moment().subtract(days, 'days').toDate(),
@@ -59,38 +53,109 @@ const getChannelVoiceActivity = (req, res) => {
       [ VoiceActivity.sequelize.literal(`DATE("jointime")`), 'ASC' ],
     ],
   }).then(result => {
+    const results = {};
+    const hours = days * 24;
+    for (let index = 0; index <= hours; index++) {
+      results[moment().subtract(index, 'hour').startOf('hour')] = {
+        users: 0,
+      };
+    }
 
-    const results = [];
+    try {
+      result.forEach(r => {
+        const join = moment(r.get('jointime')).startOf('hour');
+        const leave = moment(r.get('leavetime')).startOf('hour');
+        const duration = moment.duration(leave.diff(join));
+        const hours = duration.asHours();
 
-    for (let index = 0; index < days; index++) {
-      const day = moment().subtract(index, 'days').startOf('day').format('YYYY-MM-DD');
-      let found = false;
-
-      result.forEach(d => {
-        if (d.get('date') === day) {
-          results.unshift({
-            date: d.get('date'),
-            time: d.get('time'),
-            users: d.get('users'),
-          });
-          found = true;
+        for (let index = 0; index <= hours; index++) {
+          const t = moment(join).add(index, 'hour');
+          if (results[t]) {
+            results[t].users += 1;
+          }
         }
       });
-
-      if (!found) {
-        results.unshift({
-          date: day,
-          time: 0,
-          users: 0,
-        });
-      }
+    } catch (e) {
+      console.log(e);
     }
 
     res.status(200).send(results);
-
   }).catch(err => {
     res.status(500).send(err);
   });
+
+
+  // VoiceActivity.findAll({
+  //   attributes: [
+  //     [ VoiceActivity.sequelize.literal(`SUM("time")`), 'time' ],
+  //     [ VoiceActivity.sequelize.literal('COUNT(DISTINCT("userId"))'), 'users' ],
+  //     [ VoiceActivity.sequelize.literal('to_timestamp(floor((extract(EPOCH from jointime) / 3600 )) * 3600) AT TIME ZONE \'UTC\''), 'timejunk' ],
+  //   ],
+  //   group: [ VoiceActivity.sequelize.literal('to_timestamp(floor((extract(EPOCH from jointime) / 3600 )) * 3600) AT TIME ZONE \'UTC\'') ],
+  //   where: {
+  //     createdAt: {
+  //       [Op.gte]: moment().subtract(days, 'days').toDate(),
+  //     },
+  //     channelName: VoiceActivity.sequelize.where(VoiceActivity.sequelize.fn('LOWER', VoiceActivity.sequelize.col('channelName')), 'LIKE', '%' + channelId + '%'),
+  //   },
+  //   order: [
+  //     [ VoiceActivity.sequelize.literal('to_timestamp(floor((extract(EPOCH from jointime) / 3600 )) * 3600) AT TIME ZONE \'UTC\''), 'ASC' ],
+  //   ],
+  // }).then(result => {
+  //   res.status(200).send(result);
+  // }).catch(err => {
+  //   res.status(500).send(err);
+  // });
+
+  // VoiceActivity.findAll({
+  //   attributes: [
+  //     [ VoiceActivity.sequelize.literal(`DATE("jointime")`), 'date' ],
+  //     [ VoiceActivity.sequelize.literal(`SUM("time")`), 'time' ],
+  //     [ VoiceActivity.sequelize.literal('COUNT(DISTINCT("userId"))'), 'users' ],
+  //   ],
+  //   group: ['date'],
+  //   where: {
+  //     createdAt: {
+  //       [Op.gte]: moment().subtract(days, 'days').toDate(),
+  //     },
+  //     channelName: VoiceActivity.sequelize.where(VoiceActivity.sequelize.fn('LOWER', VoiceActivity.sequelize.col('channelName')), 'LIKE', '%' + channelId + '%'),
+  //   },
+  //   order: [
+  //     [ VoiceActivity.sequelize.literal(`DATE("jointime")`), 'ASC' ],
+  //   ],
+  // }).then(result => {
+
+  //   const results = [];
+
+  //   for (let index = 0; index < days; index++) {
+  //     const day = moment().subtract(index, 'days').startOf('day').format('YYYY-MM-DD');
+  //     let found = false;
+
+  //     result.forEach(d => {
+  //       if (d.get('date') === day) {
+  //         results.unshift({
+  //           date: d.get('date'),
+  //           time: d.get('time'),
+  //           users: d.get('users'),
+  //         });
+  //         found = true;
+  //       }
+  //     });
+
+  //     if (!found) {
+  //       results.unshift({
+  //         date: day,
+  //         time: 0,
+  //         users: 0,
+  //       });
+  //     }
+  //   }
+
+  //   res.status(200).send(results);
+
+  // }).catch(err => {
+  //   res.status(500).send(err);
+  // });
 };
 
 module.exports = { getVoice, getVoiceCount, getAverageVoiceTime, getChannelVoiceActivity };
