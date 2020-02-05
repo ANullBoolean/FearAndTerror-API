9bede8033fa01f8ab552a3432bfe3c69273a1ebc
Bryan Gillespie <bryan.gillespie@accpl.co>
2020-02-05 18:34:55 -0500

    Remove Mordhau director role, add post scriptum director role

--- routes/authentication/index.js
+++ routes/authentication/index.js
@@ -1,9 +1,9 @@
 const roles = [
   '601467946858184704', // Clan Leader
   '458087769303023617', // Clan Director
-  '525347747059204096', // Mordhau Director
   '620724284175941663', // R6S Director
   '651467080893202446', // Halo Director
+  '674759265629503498', // Post Scriptum Director
   '471757506562228235', // Tarkov Director
   '398544403314245633', // Squad Director
   '398544596755415062', // Manager
